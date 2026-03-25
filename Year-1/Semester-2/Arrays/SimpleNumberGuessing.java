32254bfe13b325ceaaed3ad0923f47650940e043
Akel <Alfredalmozara@gmail.com>
2026-03-26 00:36:09 +0800

    Simple Number Guessing Game using Math.random

---
+++ Year-1/Semester-2/Arrays/SimpleNumberGuessing.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class NumberGuessingNumber {
+    public static void main(String[] args) throws InterruptedException{
+        Scanner in = new Scanner(System.in);
+
+        String[] lowerMessage = {
+            "Lower Lods!",
+            "Ano Bayan, Lower pa!",
+            "Malay mo malapit kana Lower!",
+            "Hayst! Lower pa",
+            "Aba Aba! Lower pa"
+        };
+
+        String[] higherMessage = {
+            "Higher Po!",
+            "Siguro Taasan Mopa!",
+            "Diko Alam eh! Higher pa siguro?",
+            "Let's Go Pero mag Higher kamuna",
+            "Malapit na siguro Higher let's go!"
+        };
+
+        boolean isSecretFound = false;
+
+        
+        System.out.print("Enter 1 Up to number to guess: ");
+        int upToNumber = in.nextInt();
+        
+        final int secretNumber = (int) (Math.random() * upToNumber + 1);
+        
+        while (!isSecretFound) {
+            int lowerIndex = (int)(Math.random() * lowerMessage.length);
+            int higherIndex = (int)(Math.random() * higherMessage.length);
+            System.out.print("\nEnter Guess: ");
+            int userGuess = in.nextInt();
+            
+            if (userGuess > secretNumber) {
+                System.out.println(lowerMessage[lowerIndex]);
+            }
+            if (userGuess < secretNumber) {
+                System.out.println(higherMessage[higherIndex]);
+            }
+            if (userGuess == secretNumber) {
+                System.out.println("\nWoW You've Made it!");
+                break;
+            }
+        }
+        
+        System.out.println("\nThe Secret Number is " + secretNumber);
+
+    }
+}
