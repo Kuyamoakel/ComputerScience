dc4994c870b1b5f4acf04ac7bff2a19b759b02c7
Akel <Alfredalmozara@gmail.com>
2026-03-26 13:32:18 +0800

    Updating SecondLargest Number with manual input of array

--- Year-1/Semester-2/Arrays/SecondLargestNumber.java
+++ Year-1/Semester-2/Arrays/SecondLargestNumber.java
@@ -1,28 +1,51 @@
+import java.util.Scanner;
+
 public class SecondLargestNumber {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
+        Scanner in = new Scanner(System.in);
 
-        int[] number = {10, 45, 32, 67, 67, 12};
-        int largest = number[0];
-        int secondLargest = number[0];
-        
+        System.out.print("Enter how many numbers: ");
+        int countOfNumbers = in.nextInt();
 
-        for (int i = 1; i < number.length; i++) {
+        int[] numbers = new int[countOfNumbers];
 
-            if (number[i] > largest) {
-                secondLargest = largest;
-                largest = number[i];
-            }
-            //avoids Duplication in arrays
-            if (number[i] > secondLargest && number[i] != largest) {
-                secondLargest = number[i];
+        int max = numbers[0];
+        int secondMax = numbers[0];
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter Number " + (i+1) + ": ");
+            numbers[i] = in.nextInt();
+            if (numbers[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
             }
         }
-        System.out.println("\t===== List of the Arrays ====");
-        for (int numbers : number) {
-            System.out.print(numbers + "\t");
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                secondMax = max;
+                max = numbers[i];
+            }
+            else if (numbers[i] > secondMax && numbers[i] != secondMax) {
+                secondMax = numbers[i];
+            }
         }
-        System.out.println("\nLargest: " + largest);
-        System.out.println("SecondLargest: " + secondLargest);
+
+        System.out.println("\t========== RESULTS ==========");
+        for (int num : numbers) {
+            System.out.print(num + "\t");
+            Thread.sleep(250);
+        }
+        System.out.println("\nMax Number: " + max);
+        Thread.sleep(800);
+        System.out.println("SecondMax Number: " + secondMax);
+        Thread.sleep(800);
+        System.out.println("Count of Even: " + even);
+        Thread.sleep(800);
+        System.out.println("Count of Odd: " + odd);
         
         
 
