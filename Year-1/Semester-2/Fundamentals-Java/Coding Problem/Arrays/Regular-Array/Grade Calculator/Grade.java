1f80b839ea198f59ae1345a7e59ab28ddad315e4
Kuyamoakel <Alfredalmozara@gmail.com>
2026-05-20 21:23:30 +0800

    feat: added tenary operator

--- Year-1/Semester-2/Fundamentals-Java/Coding Problem/Arrays/Regular-Array/Grade Calculator/Grade.java
+++ Year-1/Semester-2/Fundamentals-Java/Coding Problem/Arrays/Regular-Array/Grade Calculator/Grade.java
@@ -14,17 +14,6 @@ public class Grade {
         }
 
         double average = sum / scores.length;
-
-        if (average >= 90) {
-            return "A";
-        } else if (average >= 80) {
-            return "B";
-        } else if (average >= 70) {
-            return "C";
-        } else if (average >= 60) {
-            return "D";
-        } else {
-            return "F";
-        }
+        return average > 90 ? "A" : average > 80 ? "B" : average > 70 ? "C" : average > 60 ? "D" : "F";
     }
 }
--- Year-1/Semester-2/PROJECTS/Library Management System - Java/Main.java
+++ Year-1/Semester-2/PROJECTS/Library Management System - Java/Main.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final String COLOR_YELLOW = "\u001B[33m";
+    public static final String COLOR_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Library library = new Library();
 
         while (true) {
-            System.out.print(
-                    "\n===== AKERU'S LIBRARY =====\n1. Add Book\n2. View Book\n3. Search Book\n4. Delete\n5. Restock Book\nEnter Choice: ");
+            System.out.print(COLOR_YELLOW+ "\n===== AKERU'S LIBRARY =====" + COLOR_RESET +"\n1. Add Book\n2. View Book\n3. Search Book\n4. Delete\n5. Restock Book\nEnter Choice: ");
             int menu = in.nextInt();
             in.nextLine();
 
             switch (menu) {
                 case 1:
-                    System.out.println("\n~~~ ADDING BOOKS ~~~");
+                    System.out.println(COLOR_YELLOW + "\n~~~ ADDING BOOKS ~~~" + COLOR_RESET);
                     System.out.print("Enter Book Name: ");
                     String bookN = in.nextLine();
 
@@ -28,25 +30,25 @@ public class Main {
 
                     break;
                 case 2:
-                    System.out.println("\n===== LIST OF BOOKS =====");
+                    System.out.println(COLOR_YELLOW + "\n===== LIST OF BOOKS =====" + COLOR_RESET);
                     library.viewBooks();
                     break;
                 case 3:
-                    System.out.println("\n===== SEARCHING BOOK =====");
+                    System.out.println(COLOR_YELLOW + "\n===== SEARCHING BOOK =====" + COLOR_RESET);
                     System.out.print("Enter ID: ");
                     String searchingBook = in.nextLine();
 
                     library.searchBook(searchingBook);
                     break;
                 case 4:
-                    System.out.println("\n===== DELETING BOOK =====");
+                    System.out.println(COLOR_YELLOW + "\n===== DELETING BOOK =====" + COLOR_RESET);
                     System.out.print("Enter ID: ");
                     String bookDelete = in.nextLine();
 
                     library.deleteBook(bookDelete);
                     break;
                 case 5:
-                    System.out.println("\n===== UPDATE BOOK =====");
+                    System.out.println(COLOR_YELLOW + "\n===== UPDATE BOOK =====" + COLOR_RESET);
                     System.out.print("Enter ID: ");
                     String bookUpdate = in.nextLine();
 
