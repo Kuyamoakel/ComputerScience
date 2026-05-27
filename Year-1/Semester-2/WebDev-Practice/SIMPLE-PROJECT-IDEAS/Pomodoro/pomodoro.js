2b28636e203b15ee9710b3063b78a3a63988ae0e
Kuyamoakel <Alfredalmozara@gmail.com>
2026-05-27 21:05:09 +0800

    feat: added break for 5 minutes

--- Year-1/Semester-2/WebDev-Practice/SIMPLE-PROJECT-IDEAS/Pomodoro/pomodoro.js
+++ Year-1/Semester-2/WebDev-Practice/SIMPLE-PROJECT-IDEAS/Pomodoro/pomodoro.js
@@ -1,20 +1,32 @@
 
 // variables
 const timer = document.getElementById("timer");
+const breakDisplay = document.querySelector(".break");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 const restartButton = document.getElementById("restart");
 
 // sounds
 const clickSound = new Audio("assets/audio/Click-Gun.mp3");
-const rainingSound = new Audio("assets/audio/NatureSound.mp3");
+const NatureSound = new Audio("assets/audio/NatureSound.mp3");
+const timesUp = new Audio("assets/audio/times-up.mp3");
 
 
-let timeleft = 25 * 60;
+let timeleft = 1 * 5;
+let breakTime = 1 * 10;
 let interval = null;
+let isBreak = false;
 
 function clickingSound() {
     clickSound.play();
+}
+
+function stopAllSounds() {
+    NatureSound.pause();
+    NatureSound.currentTime = 0;
+
+    timesUp.pause();
+    timesUp.currentTime = 0;
 }
 
 function updateTimer() {
@@ -28,12 +40,9 @@ function updateTimer() {
 }
 
 function startTimer() {
-    if (interval !== null) {
-        return;
-    }
-    
-    rainingSound.loop = true;
-    rainingSound.play();
+    if (interval !== null) return;
+
+    NatureSound.play();
 
     interval = setInterval(() => {
 
@@ -45,29 +54,49 @@ function startTimer() {
             clearInterval(interval);
             interval = null;
 
-            alert("POMODORO FINISHED!")
+            stopAllSounds();
+
+            switchMode();
+            startTimer();
         }
 
     }, 1000)
 }
 
 function pauseTimer() {
-    rainingSound.pause();
-    rainingSound.currentTime = 0;
+    NatureSound.pause();
+    NatureSound.currentTime = 0;
 
     clearInterval(interval);
     interval = null;
 }
 
 function resetTimer() {
-
-    rainingSound.pause();
-    rainingSound.currentTime = 0;
-    clearInterval(interval);
-    interval = null;
+    pauseTimer();
+    isBreak = false;
     timeleft = 25 * 60;
+    breakDisplay.classList.add("hidden")
+    updateTimer();
+}
 
-    timer.textContent = "25:00";
+function switchMode() {
+    isBreak = !isBreak;
+
+    if (isBreak) {
+        timeleft = breakTime;
+        breakDisplay.classList.remove("hidden");
+        breakDisplay.textContent = ("Break");
+        timesUp.play();
+    } else {
+        timeleft = 25 * 60;
+        breakDisplay.classList.add("hidden");
+
+        NatureSound.loop = true;
+        NatureSound.currentTime = 0;
+        NatureSound.play();
+    }
+
+    updateTimer();
 }
 
 startButton.addEventListener("click", startTimer);
