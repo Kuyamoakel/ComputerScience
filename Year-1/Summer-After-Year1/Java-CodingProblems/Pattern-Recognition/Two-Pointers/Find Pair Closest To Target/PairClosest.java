3041252f9e5acb387f73c35dea00324d7c1e4f2b
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-06 23:37:35 +0800

    Feat: closest into the target coding problem

---
+++ Year-1/Summer-After-Year1/Java-CodingProblems/Pattern-Recognition/Two-Pointers/Find Pair Closest To Target/PairClosest.java
@@ -0,0 +1,40 @@
+public class PairClosest {
+    public static void main(String[] args) {
+        
+        int[] nums = {1, 3, 5, 8, 10};
+        int target = 12;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        int bestLeft = nums[left];
+        int bestRight = nums[right];
+        int bestDistance = Integer.MAX_VALUE;
+        int currentDistance = 0; 
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+                
+            if (sum < target) {
+                currentDistance = target - sum;
+                if (currentDistance <= bestDistance) {
+                    bestDistance = currentDistance;
+                    bestLeft = nums[left];
+                    bestRight = nums[right];
+                }
+                left++;
+            } else {
+                currentDistance = sum - target;
+                if (currentDistance <= bestDistance) {
+                    bestDistance = currentDistance;
+                    bestLeft = nums[left];
+                    bestRight = nums[right];
+                }
+                right--;
+            }
+        }
+        
+        int newSum = bestLeft + bestRight;
+        System.out.println(bestLeft + " + " + bestRight + " = " + newSum);
+    }
+}
