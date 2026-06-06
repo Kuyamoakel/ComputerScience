91c4a40601c2da269c8c869c7acf9de621ca1d69
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-06 22:26:33 +0800

    feat: coding problem 3 two pointers done

---
+++ Year-1/Summer-After-Year1/Java-CodingProblems/Pattern-Recognition/Two-Pointers/Count Pairs With Target Sum/CountPairs.java
@@ -0,0 +1,31 @@
+public class CountPairs {
+    public static void main(String[] args) {
+
+        // Count how many pairs equal the target.
+
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        int target = 7;
+        int counter = 0;
+
+        int right = nums.length - 1;
+        int left = 0;
+
+        while (left < right) {
+            
+            int sum = nums[right] + nums[left];
+
+            if (sum == target) {
+                counter++;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        System.out.println(counter);
+
+    }
+}
