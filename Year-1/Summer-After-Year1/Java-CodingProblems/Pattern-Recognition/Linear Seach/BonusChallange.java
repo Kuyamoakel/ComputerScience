0feb1c06a458fa1915f2b61da455c67a5303cacd
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-06 21:29:47 +0800

    Feat: adding comment for two pointers

---
+++ Year-1/Summer-After-Year1/Java-CodingProblems/Pattern-Recognition/Linear Seach/BonusChallange.java
@@ -0,0 +1,15 @@
+public class BonusChallange {
+    public static void main(String[] args) {
+        
+        int[] nums = {8, 15, 23, 42, 7};
+        System.out.println(contains(nums, 23));
+
+    }
+    public static boolean contains(int[] nums, int target) {
+        for (int num : nums) {
+            if (num != target) {continue;}
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Year-1/Summer-After-Year1/Java-CodingProblems/Pattern-Recognition/Two-Pointers/Problem.java
@@ -0,0 +1,32 @@
+public class Problem {
+    public static void main(String[] args) {
+
+        
+        int[] nums = {1, 2, 3, 4, 6, 8, 11};
+
+        int target = 10;
+        int left = 0;
+        int right = nums.length - 1;
+        boolean isFound = false;
+
+        while (left < right) {
+
+            int sum = nums[left] + nums[right];
+
+            if (target == sum) {
+                System.out.println(isFound = true);
+                System.out.println(nums[left] + " + " + nums[right] + " = " + sum);
+                return;
+            }
+
+            if (target > sum) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println(isFound);
+
+    }
+}
