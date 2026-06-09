c054b5e33e3c992e2fe3e3f1b01aa3155c2ba76a
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-09 22:25:56 +0800

    feat: modify abstract coding practice #1

--- Year-1/Summer/Object Oriented Programming - Java/Abstraction/Characters/Characters.java
+++ Year-1/Summer/Object Oriented Programming - Java/Abstraction/Characters/Characters.java
@@ -2,9 +2,17 @@ abstract class Character {
 
     abstract void attack();
     abstract void shielding();
+    abstract int getHealth();
+    abstract String getClassname();
 
+
+    // Concreate Method
     void move() {
-        System.out.println("Characer Moves");
+        System.out.println("Characer Moves...");
+    }
+
+    void rest() {
+        System.out.println("Character Rest...");
     }
 }
 
@@ -21,6 +29,17 @@ class Mage extends Character {
     void shielding() {
         System.out.println("Mage is Proctecting herself!");
     }
+
+    @Override
+    public int getHealth() {
+        return 85;
+    }
+
+    @Override
+    public String getClassname() {
+        return "Mage";
+    }
+    
 }
 
 class Tank extends Character {
@@ -34,23 +53,59 @@ class Tank extends Character {
     void shielding() {
         System.out.println("Tank raise his Shield!");
     }
+
+    @Override
+    public int getHealth() {
+        return 150;
+    }
+
+    @Override
+    public String getClassname() {
+        return "Tank";
+    }
+}
+
+class Archer extends Character {
+    
+    @Override
+    void attack() {
+        System.out.println("Archer attacking the tower!");
+    }
+    
+    @Override
+    void shielding() {
+        System.out.println("Archer goes to back of the Tank!");
+    }
+
+    @Override
+    public int getHealth() {
+        return 100;
+    }
+
+    @Override
+    public String getClassname() {
+        return "Archer";
+    }
 }
 
 public class Characters {
     public static void main(String[] args) {
 
-        Character mage = new Mage();
-        Character tank = new Tank();
+        Character[] heros = {
+            new Mage(),
+            new Tank(),
+            new Archer()
+        };
 
-        mage.move();
-        tank.move();
-
-        System.out.println();
-
-        mage.attack();
-        mage.shielding();
-        tank.attack();
-        tank.shielding();
+        for (Character character : heros) {
+            System.out.println("Character: " + character.getClassname());
+            System.out.println("Health: " + character.getHealth());
+            character.move();
+            character.attack();
+            character.shielding();
+            character.rest();
+            System.out.println();
+        }
         
     }
 }
