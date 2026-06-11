615892cefe3951c5dea53c2f134089807520acf7
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-11 23:28:35 +0800

    feat: oppsite direction two pointers/counter pairs that is greater than the target

---
+++ Year-1/Summer/Java-CodingProblems/Two-Pointers/Opposite-Direction/Count Pairs Greater Than Target/GreaterThanTarget.java
@@ -0,0 +1,30 @@
+public class GreaterThanTarget {
+    public static void main(String[] args) {
+        
+        int[] num = {1, 2, 3, 4, 5};
+
+        int target = 7;
+        int counter = 0;
+
+        int left = 0;
+        int right = num.length - 1;
+
+        while (left < right) {
+
+            int sum = num[left] + num[right];
+
+            if (sum > target) {
+                counter++;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                counter++;
+                right--;
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
