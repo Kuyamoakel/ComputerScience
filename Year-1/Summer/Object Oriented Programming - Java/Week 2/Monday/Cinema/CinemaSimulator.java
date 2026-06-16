a48309c65acae74373b3cae78024ca3017216c80
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-16 23:15:04 +0800

    feat: added major functionallity, next step the rules

--- Year-1/Summer/Object Oriented Programming - Java/Week 2/Monday/Cinema/CinemaSimulator.java
+++ Year-1/Summer/Object Oriented Programming - Java/Week 2/Monday/Cinema/CinemaSimulator.java
@@ -22,9 +22,23 @@ class Cinema {
         bookings.add(booking);
     }
 
+    Movie getMovie(int index) {
+        return movies.get(index);
+    }
+
     void viewAllMovies() {
+        System.out.println("\n===== LIST OF MOVIES =====");
         for (int i = 0; i < movies.size(); i++) {
-            System.out.println(i + 1 + ". " + movies.get(i).name);
+            System.out.println(i + 1 + ". " + movies.get(i));
+            System.out.println();
+        }
+    }
+
+    void viewBookingTickets() {
+        System.out.println("===== LSIT OF BOOKINGS =====");
+        for (int i = 0; i < movies.size(); i++) {
+            System.out.println(i + 1 + ". " + bookings.get(i));
+            System.out.println();
         }
     }
 }
@@ -86,10 +100,6 @@ class Booking {
         this.movie = movie;
     }
 
-    void viewBookingDetails() {
-        System.out.println(this);
-    }
-
     @Override
     public String toString() {
         return "Booking ID: " + bookingId +
@@ -119,14 +129,70 @@ class Manager {
 
 public class CinemaSimulator {
     public static void main(String[] args) {
-        Cinema cinema = new Cinema();
-        Movie movie1 = new Movie("Avingir", "Action", 150, 50);
-        Customer cos1 = new Customer("Akel", "AK12");
-        Booking book1 = new Booking("AK12", 5, "6/16/26", cos1, movie1);
+        Scanner in = new Scanner(System.in);
 
-        cinema.addMovie(movie1);
-        cinema.addCostumer(cos1);
-        cinema.addBooking(book1);
-        cinema.viewAllMovies();
-    }
+        Cinema cinema = new Cinema();
+        Movie movie;
+        Booking book;
+        Customer customer;
+
+        while (true) {
+            System.out.print("[1] View Movies\n[2] Add Movie\n[3] Book Tickets\n[4] View Bookings\n[5] Exit\nChoices: ");
+            int menu = in.nextInt();
+            in.nextLine();
+
+            switch (menu) {
+                case 1 -> {cinema.viewAllMovies();}
+                case 2 -> {
+                    System.out.println("==== ADD MOVIE ====");
+                    System.out.print("Movie Name: ");
+                    String movieName = in.nextLine();
+
+                    System.out.print("Genre: ");
+                    String movieGenre = in.nextLine();
+
+                    System.out.print("Ticket Price: ");
+                    int movieTicketPrice = in.nextInt();
+
+                    System.out.print("Available Sets: ");
+                    int movieAvailableSet = in.nextInt();
+
+                    movie = new Movie(movieName, movieGenre, movieTicketPrice, movieAvailableSet);
+
+                    cinema.addMovie(movie);
+                }
+                case 3 -> {
+                    System.out.println("==== BOOK TICKETS ====");
+                    System.out.print("Booking ID: ");
+                    String bookingID = in.nextLine();
+
+                    System.out.print("Number Of Tickets: ");
+                    int numberOfTickets = in.nextInt();
+                    in.nextLine();
+
+                    System.out.print("Booking Date (ex. 6/16/2026): ");
+                    String bookingDate = in.nextLine();
+
+                    System.out.print("Customer Name: ");
+                    String customerName = in.nextLine();
+
+                    System.out.print("Customer ID: ");
+                    String customerID = in.nextLine();
+
+                    cinema.viewAllMovies();
+
+                    System.out.print("\nEnter Movie Number: ");
+                    int movieNumber = in.nextInt();
+                    
+                    customer = new Customer(customerName, customerID);
+                    book = new Booking(bookingID, numberOfTickets, bookingDate, customer, cinema.getMovie(movieNumber - 1));
+                    cinema.addBooking(book);
+                }
+                case 4 -> {
+                    book.viewBookingDetails();
+                }
+                default -> {}
+            }
+        }
+    } 
 }
