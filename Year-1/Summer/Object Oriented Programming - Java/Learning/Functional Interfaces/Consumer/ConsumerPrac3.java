b2a72d1da9075a6279dfa2deb93666e8494d7e63
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-23 00:14:09 +0800

    feat: ConsumerPrac3, List of names printed using foreach

---
+++ Year-1/Summer/Object Oriented Programming - Java/Learning/Functional Interfaces/Consumer/ConsumerPrac3.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerPrac3 {
+    public static void main(String[] args) {
+        
+        List<String> usernames = List.of("Akel", "Steph", "Aira", "Solomon");
+        Consumer<String> display = name -> System.out.println("User: " + name);
+        usernames.forEach(display);
+
+        
+    }
+}
