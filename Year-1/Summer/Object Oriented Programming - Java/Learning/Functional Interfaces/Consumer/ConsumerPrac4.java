4bcdb77749ddd45f19bd27fb79b7535210804a23
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-23 00:21:49 +0800

    feat: ConsumerPrac4 -> Chained Actions, combining two Consumer into one the idea

--- Year-1/Summer/Object Oriented Programming - Java/Learning/Functional Interfaces/Consumer/ConsumerPrac4.java
+++ Year-1/Summer/Object Oriented Programming - Java/Learning/Functional Interfaces/Consumer/ConsumerPrac4.java
@@ -3,11 +3,15 @@ import java.util.function.Consumer;
 public class ConsumerPrac4 {
     public static void main(String[] args) {
         
-        Consumer<String> database = data -> System.out.println("Saving Record: " + data);
+        Consumer<String> Processing = data -> System.out.println("Processing: " + data);
+        Consumer<String> Completed = data -> System.out.println("Completed: " + data);
+        Consumer<String> Combined = Processing.andThen(Completed);
+        
 
-        database.accept("Akel");
-        database.accept("Steph");
-        database.accept("Aira");
-        database.accept("Solomon");
+        Combined.accept("Akel");
+        Combined.accept("Steph");
+        Combined.accept("Aira");
+        Combined.accept("Solomon");
+
     }
 }
