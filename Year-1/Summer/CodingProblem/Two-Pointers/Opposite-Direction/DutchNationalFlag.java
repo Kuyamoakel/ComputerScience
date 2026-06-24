e39ef1c9f8cbed0c4e07959798717c4d00130b59
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-24 21:56:34 +0800

    feat: solve Dutch National Flag using opposite-direction pointers

---
+++ Year-1/Summer/CodingProblem/Two-Pointers/Opposite-Direction/DutchNationalFlag.java
@@ -0,0 +1,40 @@
+public class DutchNationalFlag {
+    public static void main(String[] args) {
+        int[] colors = {2, 2, 1, 0, 0, 1, 2, 0};
+
+        sortColors(colors);
+    }
+    static void sortColors(int[] colors) {
+        int current = 0;
+        int right = 0;
+        int left = colors.length - 1;
+
+        while (current <= left) {
+
+            if (colors[current] == 0) {
+                // A must step to save the previous value in the array.
+                int temp = colors[right];
+                colors[right] = colors[current];
+                colors[current] = temp;
+                
+                current++;
+                right++;
+            }
+
+            else if (colors[current] == 1) {
+                current++;
+            }
+
+            else {
+                int temp = colors[left];
+                colors[left] = colors[current];
+                colors[current] = temp;
+
+                left--;
+            }
+        }
+        for (int color : colors) {
+            System.out.print(color + " ");
+        }
+    }
+} 
