eea072dd2ef64cf71d39db0c5301626df2594853
Kuyamoakel <Alfredalmozara@gmail.com>
2026-06-29 22:26:53 +0800

    feat: solve Boats to Save People using two pointers Opposite Direction

---
+++ Year-1/Summer/CodingProblem/Two-Pointers/Opposite-Direction/BoatsToSavePeople.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+    public static void main(String[] args) {
+
+        int[] people = {3, 2, 2, 1};
+        int limit = 3;
+
+        System.out.println(numberOfResueBoats(people, limit));
+
+    }
+
+    static int numberOfResueBoats(int[] people, int limit) {
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+
+        while (left <= right) {
+
+            if (people[left] + people[right] <= limit) {
+                right--;
+                left++;
+            }
+            else {
+                right--;
+            }
+
+            boats++;
+        }
+
+        return boats;
+        
+    } 
+}
