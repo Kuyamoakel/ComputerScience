b30f882d135e5f6904117cc4fbb31773525b1d6e
Kuyamoakel <Alfredalmozara@gmail.com>
2026-09-01 17:09:20 +0800

    feat: case CLI progress done with adding musicians and soon the case 3

--- Year-2/Semester 1/Projects/Musiched/Main.java
+++ Year-2/Semester 1/Projects/Musiched/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
         // Lists
         List<Song> songs = new ArrayList<>();
+        List<Musician> musicians = new ArrayList<>();
 
         // Array
         MusicalKey[] keys = MusicalKey.values();
@@ -18,14 +19,13 @@ public class Main {
             try {
 
                 System.out.print(
-                    "----- Worship Library ----\n" +
-                    "1. Add song\n" +
-                    "2. Add musician\n" +
-                    "3. Build a worship set\n" +
-                    "4. View Library\n" +
-                    "5. Exit\n" +
-                    "Choice: "
-                );
+                        "----- Worship Library ----\n" +
+                                "1. Add song\n" +
+                                "2. Add musician\n" +
+                                "3. Build a worship set\n" +
+                                "4. View Library\n" +
+                                "5. Exit\n" +
+                                "Choice: ");
                 int menu = input.nextInt();
                 input.nextLine();
 
@@ -41,8 +41,8 @@ public class Main {
                         String artist = input.nextLine();
 
                         System.out.println("Select Key: ");
-                        for(int i = 0; i < keys.length; i++) {
-                            System.out.println((i+1) + ". " + keys[i]);
+                        for (int i = 0; i < keys.length; i++) {
+                            System.out.println((i + 1) + ". " + keys[i]);
                         }
 
                         System.out.print("Choice: ");
@@ -51,8 +51,8 @@ public class Main {
 
                         MusicalKey selectedKey = keys[keyChoice - 1];
 
-                        System.out.println("Add Chords and Label to it (Stanza, Pre, Chorus):");
-                        boolean decision;
+                        System.out.println("Add Chords and Label it (Stanza, Pre, Chorus):");
+                        boolean chordLoop = false;
                         do {
                             System.out.print("Enter chord: ");
                             String chord = input.nextLine();
@@ -62,14 +62,73 @@ public class Main {
 
                             System.out.print("Do you want to continue? (Y / N): ");
                             String continuing = input.nextLine().toLowerCase();
-                            
-                            decision = (continuing.equals("y"));
-                            
-                        } while (decision);
 
-                        Song newSong = new Song(title, artist, selectedKey, chords);
-                        songs.add(newSong);
-                        System.out.println("Song Added\n");
+                            chordLoop = (continuing.equals("y"));
+
+                        } while (chordLoop);
+
+                        System.out.print("\nConfirmend (Y/N): ");
+                        String confirmationCaseOne = input.nextLine();
+
+                        if (confirmationCaseOne.equalsIgnoreCase("y")) {
+                            Song newSong = new Song(title, artist, selectedKey, chords);
+                            songs.add(newSong);
+                            System.out.println("Song Added\n");
+                        } else if (confirmationCaseOne.equalsIgnoreCase("n")) {
+                            System.out.println("Canceled!");
+                            break;
+                        } else {
+                            System.out.println("Invalid Input");
+                            break;
+                        }
+                        break;
+                    case 2:
+                        List<Instrument> instruments = new ArrayList<>();
+                        System.out.println("--- Adding Musicians ---");
+
+                        System.out.print("Enter Name: ");
+                        String name = input.nextLine();
+
+                        System.out.print("Enter Age: ");
+                        int age = input.nextInt();
+                        input.nextLine();
+                        System.out.println("--- Insert your Instruments ---");
+
+                        boolean instrumentLoop = false;
+                        do {
+                            System.out.print("Instrument Name: ");
+                            String instrumentName = input.nextLine();
+
+                            System.out.print("Instrument Type: ");
+                            String instrumentType = input.nextLine();
+
+                            if (instrumentName.equalsIgnoreCase(instrumentType)) {
+                                System.out.println("Error! Try again... must not be same name!");
+                                instrumentLoop = true;
+                            } else {
+                                Instrument musicInstrument = new Instrument(instrumentName, instrumentType);
+                                instruments.add(musicInstrument);
+                                System.out.print("Add more? (Y/N): ");
+                                String continuing = input.nextLine().toLowerCase();
+                                instrumentLoop = (continuing.equals("y"));
+                            }
+
+                        } while (instrumentLoop);
+
+                        System.out.print("\nConfirmend (Y/N): ");
+                        String confirmationCaseTwo = input.nextLine();
+
+                        if (confirmationCaseTwo.equalsIgnoreCase("y")) {
+                            Musician newMusician = new Musician(name, age, instruments);
+                            musicians.add(newMusician);
+                            System.out.println("Musician Added!\n");
+                        } else if (confirmationCaseTwo.equalsIgnoreCase("n")) {
+                            System.out.println("Canceled!");
+                            break;
+                        } else {
+                            System.out.println("Invalid Input");
+                            break;
+                        }
                         break;
                     case 5:
                         System.out.println("Exiting Program...");
